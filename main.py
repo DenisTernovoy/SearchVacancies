d71c0ac9f8a4998f98e9b0ef7b706d6549f8b437
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-08-16 16:02:32 +0300

    class JSONSaver added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pathlib
+
 import requests
 import pprint
 from src.api_hh import HH
+from src.files import JSONSaver
 from src.vacancies import Vacancy
 
 
@@ -8,10 +11,10 @@ def connect() -> int:
     hh_connect = HH()
     hh_vacancies = hh_connect.get_vacancies("Микробиолог")
     list_vacancies = Vacancy.cast_to_object_list(hh_vacancies)
-    pprint.pprint(Vacancy.cast_to_dict_format(list_vacancies[5:7]))
-    # pprint.pprint(hh_vacancies[1:5])
-    a, b = list_vacancies[5:7]
-    print(a < 1000000)
+    list_dict_format = Vacancy.cast_to_dict_format(list_vacancies)
+    js = JSONSaver()
+    js.write_data(list_dict_format)
+
 
 connect()
 
